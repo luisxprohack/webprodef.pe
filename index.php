401c85483d87732a91c84c7d4589d5305302df4c
luis salazar <luis.hack10@gmail.com>
2023-01-12 12:13:20 -0500

    intro y servicios
    crear todas las carpetas .....

---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PRODEF</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="piura,prestamos,credi amigo,credito financiero,bajos intereses,sullana,chimbote,chiclayo,trujillo,lima,san juan de miraflores,peru,rapido,facil" name="keywords" />
+    <meta content="Crediamigo es una empresa peruana que ofrece prestamos personales con bajos intereses y en el menor tiempo posible." name="description" />
+
+    <!--favicon-->
+    <link href="img/logo.svg" rel="icon" />
+    <link href="img/logo.svg" rel="apple-touch-icon" />
+    <!--font awesome-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,500,600,700,700i|Montserrat:300,400,500,600,700" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
+
+    <!-- Bootstrap CSS File -->
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- Libraries CSS Files -->
+    <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="lib/animate/animate.min.css" rel="stylesheet" />
+    <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet" />
+
+    <!-- =======================================================
+    Theme Name: Rapid
+    Theme URL: https://bootstrapmade.com/rapid-multipurpose-bootstrap-business-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+
+</head>
+<header id="header">
+    <div id="topbar">
+        <div class="container">
+            <div class="social-links">
+                <a href="#" class="facebook">
+                    <i class="fa fa-facebook"></i>
+                </a>
+                <a href="#" class="instagram">
+                    <i class="fa fa-instagram"></i>
+                </a>
+                <a href="#" class="linkedin">
+                    <i class="fa fa-linkedin"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="logo float-left">
+            <h1 class="text-light"><a href="#intro" class="scrollto"><span>PRODEF</span></a></h1>
+        </div>
+        <nav class="main-nav float-right d-none d-lg-block">
+            <ul>
+                <li class="active"><a href="#intro">Inicio</a></li>
+                <li><a href="#services">Servicios</a></li>
+                <li><a href="#services">Nosotros</a></li>
+            </ul>
+        </nav><!-- .main-nav -->
+    </div>
+</header><!-- #header -->
+
+<!--==========================
+    Intro Section
+  ============================-->
+<section class="clearfix" id="intro">
+    <div class="container d-flex h-100">
+        <div class="row justify-content-center align-self-center">
+            <div class="col-md-6 intro-info order-md-first order-last">
+                <h2>Prestamos rapidos y <br>de bajo intereses para su <span>negocio</span></h2>
+                <div>
+                    <a href="#about" class="btn-get-started scrollto">Saber más</a>
+                </div>
+            </div>
+            <div class="col-md-6 intro-img order-md-last order-first">
+                <img src="img/intro-img.svg" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section><!-- #intro -->
+
+<main id="main">
+    <!--==========================
+    Services Section
+  ============================-->
+    <section id="services" class="section-bg">
+        <div class="container">
+            <header class="section-header">
+                <h3>Servicios</h3>
+                <p>estos son los servicios que ofreces aqui en PRODEF</p>
+            </header>
+            <div class="row">
+                <div class="col-md-6 col-lg-4 wow bounceInup" data-wow-duration="1.4s">
+                    <div class="box">
+                        <div class="icon" style="background: #F2F2F2;"><i class="fa fa-cog" style="color: #1B2459;"></i></div>
+                        <h4 class="title"><a href="">Préstamos personalizados</a></h4>
+                        <p class="description text-justify">
+                            Ofrecemos préstamos adaptados a las necesidades específicas de cada microempresa. Esto incluye opciones de plazo de pago y tasas de interés flexibles.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 wow bounceInup" data-wow-duration="1.4s">
+                    <div class="box">
+                        <div class="icon" style="background: #F2F2F2;"><i class="fas fa-headset" style="color: #1B2459;"></i></div>
+                        <h4 class="title"><a href="">Servicio al cliente excepcional</a></h4>
+                        <p class="description text-justify">
+                        Nuestro equipo de servicio al cliente está comprometido a brindar un servicio excepcional y está disponible para responder cualquier pregunta o inquietud que pueda tener
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 wow bounceInup" data-wow-duration="1.4s">
+                    <div class="box">
+                        <div class="icon" style="background: #F2F2F2;"><i class="fas fa-clock" style="color: #1B2459;"></i></div>
+                        <h4 class="title"><a href="">Rapidez en la aprobación</a></h4>
+                        <p class="description text-justify">
+                        Nos esforzamos por tomar decisiones de aprobación rápidas para que las microempresas puedan obtener el financiamiento que necesitan en un plazo de tiempo razonable..
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 wow bounceInup" data-wow-duration="1.4s">
+                    <div class="box">
+                        <div class="icon" style="background: #F2F2F2;"><i class="fas fa-lock" style="color: #1B2459;"></i></div>
+                        <h4 class="title"><a href="">Seguridad y confidencialidad</a></h4>
+                        <p class="description text-justify">
+                        Tratamos toda la información de nuestros clientes con la máxima confidencialidad y seguridad.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 wow bounceInup" data-wow-duration="1.4s">
+                    <div class="box">
+                        <div class="icon" style="background: #F2F2F2;"><i class="fas fa-credit-card" style="color: #1B2459;"></i></div>
+                        <h4 class="title"><a href="">Pagos flexibles</a></h4>
+                        <p class="description text-justify">
+                        Ofrecemos diferentes opciones de pago para adaptarse a las necesidades de su negocio.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 wow bounceInup" data-wow-duration="1.4s">
+                    <div class="box">
+                        <div class="icon" style="background: #F2F2F2;"><i class="fas fa-info-circle" style="color: #1B2459;"></i></div>
+                        <h4 class="title"><a href="">Sin gastos ocultos</a></h4>
+                        <p class="description text-justify">
+                        No cobramos comisiones o gastos ocultos, lo que le permite saber exactamente cuánto debe pagar
+                        </p>
+                    </div>
+                </div>               
+            </div>
+        </div>
+    </section><!--end services section-->
+
+    <!-- why us section -->
+    
+
+</main> <!--main-->
+<a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/mobile-nav/mobile-nav.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+
+<body>
+
+</body>
+
+</html>
